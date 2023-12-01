d1bcc2fbdb57ab3bdcac5f76b64252b273615600
everton_goncalves <everton.goncalves.89@gmail.com>
2023-12-01 13:54:41 -0300

    test: parameterized test

--- src/test/java/com/egon/msscbrewery/beer/controllers/CreateBeerControllerTest.java
+++ src/test/java/com/egon/msscbrewery/beer/controllers/CreateBeerControllerTest.java
@@ -5,11 +5,16 @@ import com.egon.msscbrewery.beer.helpers.BeerDtoHelper;
 import com.egon.msscbrewery.beer.services.CreateBeerService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.stream.Stream;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -47,9 +52,12 @@ class CreateBeerControllerTest {
     verify(service, times(1)).execute(beerDto);
   }
 
-  @Test
-  void shouldReturnBadRequestErrorWhenFieldsAreInvalid() throws Exception {
-    var beerDto = BeerDto.builder().build();
+  /*
+  * spring-boot-starter-test already has the junit-jupiter-params artifact
+  */
+  @ParameterizedTest
+  @MethodSource("provideInvalidFields")
+  void shouldReturnBadRequestErrorWhenFieldsAreInvalid(BeerDto beerDto) throws Exception {
     var beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
     mockMvc.perform(post("/api/v1/beers")
@@ -57,5 +65,17 @@ class CreateBeerControllerTest {
                     .content(beerDtoJson))
             .andExpect(status().isUnprocessableEntity());
     verify(service, never()).execute(any());
+  }
+
+  private static Stream<Arguments> provideInvalidFields() {
+    return Stream.of(
+            Arguments.of(BeerDto.builder().name(null).style("style").upc(1L).build()),
+            Arguments.of(BeerDto.builder().name("").style("style").upc(1L).build()),
+            Arguments.of(BeerDto.builder().name("name").style(null).upc(1L).build()),
+            Arguments.of(BeerDto.builder().name("name").style("").upc(1L).build()),
+            Arguments.of(BeerDto.builder().name("name").style("style").upc(null).build()),
+            Arguments.of(BeerDto.builder().name("name").style("style").upc(Long.MIN_VALUE).build()),
+            Arguments.of(BeerDto.builder().name("name").style("style").upc(0L).build()),
+            Arguments.of(BeerDto.builder().build()));
   }
 }
