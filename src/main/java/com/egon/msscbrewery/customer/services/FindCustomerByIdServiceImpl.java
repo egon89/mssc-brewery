4c0c62f4436f310d53bb8915d5de70e0eab653af
everton_goncalves <everton.goncalves.89@gmail.com>
2023-12-08 09:39:12 -0300

    feat: inject mapper into service

--- src/main/java/com/egon/msscbrewery/customer/services/FindCustomerByIdServiceImpl.java
+++ src/main/java/com/egon/msscbrewery/customer/services/FindCustomerByIdServiceImpl.java
@@ -1,18 +1,28 @@
 package com.egon.msscbrewery.customer.services;
 
 import com.egon.msscbrewery.customer.dtos.CustomerDto;
+import com.egon.msscbrewery.customer.entities.CustomerEntity;
+import com.egon.msscbrewery.customer.mappers.CustomerMapper;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class FindCustomerByIdServiceImpl implements FindCustomerByIdService {
+
+  private final CustomerMapper mapper;
+
   @Override
   public CustomerDto execute(@NonNull UUID id) {
-    return CustomerDto.builder()
-        .id(id)
-        .name("Customer 1")
-        .build();
+    val customer = CustomerEntity.builder()
+            .id(id)
+            .name("Customer 1")
+            .build();
+
+    return mapper.toDto(customer);
   }
 }
