33d0b341a32e5cece9f9c64614bdf8368be50a61
Everton Goncalves <everton.goncalves.89@gmail.com>
2023-12-17 23:25:05 -0300

    test: deserialize test - jackson

--- src/test/java/com/egon/msscbrewery/beer/dtos/BeerDtoSnakeTest.java
+++ src/test/java/com/egon/msscbrewery/beer/dtos/BeerDtoSnakeTest.java
@@ -14,15 +14,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("snake")
 @JsonTest
 class BeerDtoSnakeTest {
+  public static final String JSON_VALUE = "{\"id\":\"aeb60d96-6aa1-4c2e-a82b-1683a7439d47\",\"name\":\"Beer 1\",\"style\":\"Style 1\",\"upc\":345678,\"created_at\":null,\"updated_at\":null}";
   @Autowired
   ObjectMapper objectMapper;
 
   @Test
   void shouldSerialize() throws JsonProcessingException {
     val dto = BeerDtoHelper.create();
-    val expected = "{\"id\":\"aeb60d96-6aa1-4c2e-a82b-1683a7439d47\",\"name\":\"Beer 1\",\"style\":\"Style 1\",\"upc\":345678,\"created_at\":null,\"updated_at\":null}";
 
     val result = objectMapper.writeValueAsString(dto);
+
+    assertThat(result).isEqualTo(JSON_VALUE);
+  }
+
+  @Test
+  void shouldDeserialize() throws JsonProcessingException {
+    val expected = BeerDtoHelper.create();
+
+    val result = objectMapper.readValue(JSON_VALUE, BeerDto.class);
 
     assertThat(result).isEqualTo(expected);
   }
